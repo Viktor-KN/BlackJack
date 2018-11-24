6623df667a3ca4549514bba78c86ff03f716b6a2
Viktor Kachulin <vicart2008@gmail.com>
2018-11-24 16:49:56 +1000

    Added Bank class prototype

---
+++ bank.rb
@@ -0,0 +1,22 @@
+class Bank
+  def initialize
+  end
+
+  def add_player(name, balance)
+  end
+
+  def make_bets(amount)
+  end
+
+  def winner(name)
+  end
+
+  def tie
+  end
+
+  def bets
+  end
+
+  def balance(name)
+  end
+end
