1744892f1f41f4f705e4b3c4b60871a085c8eb71
Viktor Kachulin <vicart2008@gmail.com>
2018-11-24 17:55:32 +1000

    Added methods for Bank class

--- bank.rb
+++ bank.rb
@@ -1,22 +1,39 @@
 class Bank
   def initialize
+    @players = {}
+    @bets = Hash.new(0)
   end
 
   def add_player(name, balance)
+    players[name] = balance unless players.key?(name)
   end
 
   def make_bets(amount)
+    players.each_key do |name|
+      players[name] -= amount
+      bets[name] += amount
+    end
   end
 
   def winner(name)
+    players[name] += bets_amount
+    bets.clear
   end
 
   def tie
-  end
-
-  def bets
+    bets.each { |name, amount| players[name] += amount }
+    bets.clear
   end
 
   def balance(name)
+    players[name]
   end
+
+  def bets_amount
+    bets.values.sum
+  end
+
+  private
+
+  attr_reader :players, :bets
 end
