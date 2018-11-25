058851ffeadf3734969f77f6a3aaf479a16cb603
Viktor Kachulin <vicart2008@gmail.com>
2018-11-25 23:10:44 +1000

    Added Player class

---
+++ player.rb
@@ -0,0 +1,17 @@
+class Player
+  attr_reader :name, :cards
+
+  def initialize(name)
+    @name = name
+    @cards = []
+  end
+
+  def turn
+  end
+
+  def points
+  end
+
+  def new_round
+  end
+end
