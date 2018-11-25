521d3fec2854964560889d1ade4c96986874f2f7
Viktor Kachulin <vicart2008@gmail.com>
2018-11-26 01:23:23 +1000

    Changed Deck class to to define card points on creation

--- deck.rb
+++ deck.rb
@@ -8,7 +8,11 @@ class Deck
     cards.pop
   end
 
-  def shuffle_deck!
+  def return_cards(cards)
+    cards.push(*cards)
+  end
+
+  def shuffle!
     cards.shuffle!
   end
 
@@ -24,9 +28,27 @@ class Deck
     %w[2 3 4 5 6 7 8 9 10 J Q K A]
   end
 
+  def card_points
+    {
+      '2' => { value: 2 },
+      '3' => { value: 3 },
+      '4' => { value: 4 },
+      '5' => { value: 5 },
+      '6' => { value: 6 },
+      '7' => { value: 7 },
+      '8' => { value: 8 },
+      '9' => { value: 9 },
+      '10' => { value: 10 },
+      'J' => { value: 10 },
+      'Q' => { value: 10 },
+      'K' => { value: 10 },
+      'A' => { value: 11, overflow: 11, overflow_value: 1 }
+    }
+  end
+
   def fill_deck
     card_suits.each do |suit|
-      card_ranks.each { |rank| cards << Card.new(rank, suit) }
+      card_ranks.each { |rank| cards << Card.new(rank, suit, card_points[rank]) }
     end
   end
 end
