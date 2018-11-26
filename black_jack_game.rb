92aedbc1c2a36b0f451f9f93fb08e4569b290772
Viktor Kachulin <vicart2008@gmail.com>
2018-11-27 01:38:49 +1000

    Changed BlackJackGame class: more methods

--- black_jack_game.rb
+++ black_jack_game.rb
@@ -45,9 +45,6 @@ class BlackJackGame
     3
   end
 
-  def variant
-  end
-
   def init
     self.bank = Bank.new
     self.deck = Deck.new
@@ -63,7 +60,7 @@ class BlackJackGame
   def new_round
     players.each do |player|
       if bank.balance(player.name) < bet_amount
-        return fsm.active_state = player.is_a?(DealerPlayer) ? :dealer_loose : :player_loose
+        return fsm.active_state = player.dealer? ? :dealer_loose : :player_loose
       end
 
       deck.return_cards(player.hand.cards)
@@ -77,18 +74,54 @@ class BlackJackGame
     puts 'Making bets.'
     bank.make_bets(bet_amount)
 
+
     fsm.active_state = :round_loop
   end
 
   def deal_cards
-    init_cards.times do
-      players.each do |player|
-        player.hand.cards << deck.get_card
+    init_cards.times { players.each(&method(:take_card)) }
+  end
+
+  def show_player_info(opts = {})
+    players.reverse_each do |player|
+      if player.dealer? && opts[:hide_cards_points] == true
+        cards = player.hand.show_cards(hide_cards: true)
+        points = 'XX'
+      else
+        cards = player.hand.show_cards
+        points = player.hand.points
       end
+      puts player.name
+      printf("  Cards: %-12sPoints: %-4sBet: %-6sBalance: %s\n",
+             cards, points, bank.bet(player.name), bank.balance(player.name))
     end
   end
 
   def round_loop
+    show_player_info(hide_cards_points: true)
+    variant = nil
+    players.each do |player|
+      return fsm.active_state = :end_round unless player.dealer? || player.hand.points <= 21
 
+      variant = player.turn
+      player.variants.delete(variant)
+      puts "#{player.name} choose to #{variant[:title]}"
+      send variant[:action], player
+    end
+    sleep 2
+  end
+
+  def take_card(player)
+    card = deck.get_card
+    puts "#{player.name} received new card: #{player.dealer? ? card.to_s(hide_cards: true) : card}"
+    player.hand.cards << card
+  end
+
+  def pass_turn(player)
+
+  end
+
+  def open_cards(_player)
+    fsm.active_state = :end_round
   end
 end
