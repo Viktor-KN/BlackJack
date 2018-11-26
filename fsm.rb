28807401a6b0907d5a7c8a1ebab6ca1c4c049faa
Viktor Kachulin <vicart2008@gmail.com>
2018-11-26 15:36:20 +1000

    Added FSM class: simple Final State Machine

---
+++ fsm.rb
@@ -0,0 +1,15 @@
+class FSM
+  attr_accessor :active_state
+
+  def initialize(caller)
+    @caller = caller
+  end
+
+  def update
+    caller.send active_state
+  end
+
+  private
+
+  attr_reader :caller
+end
