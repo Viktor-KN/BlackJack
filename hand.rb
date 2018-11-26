557cc53b177ae930f785467f8b1ecd8add9644be
Viktor Kachulin <vicart2008@gmail.com>
2018-11-27 01:13:07 +1000

    Changed Hand class: modified points calc, new to_s method

--- hand.rb
+++ hand.rb
@@ -6,6 +6,21 @@ class Hand
   end
 
   def points
+    points = calc_points(cards)
+    if points > 21
+      alt_points = calc_points(cards.reverse)
+      return [points, alt_points].min
+    end
+    points
+  end
+
+  def show_cards(opts = {})
+    cards.reduce('') { |memo, card| "#{memo}#{memo.empty? ? '' : '  '}#{card.to_s(opts)}" }
+  end
+
+  private
+
+  def calc_points(cards)
     cards.reduce(0) do |sum, card|
       card_points = if card.points.key?(:overflow) && sum >= card.points[:overflow]
                       card.points[:overflow_value]
